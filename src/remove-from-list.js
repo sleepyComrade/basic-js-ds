92906e610e650002e2bc52c0e0b9180ff8142507
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-10-16 09:22:41 +0900

    complete remove from list subtask

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const removeKFromList = (l, k) => {
+  let node = l;
+  let prev;
+  let isNotDone = true;
+  while (isNotDone) {
+    while (node.value !== k) {
+      if (node.next !== null) {
+        prev = node;
+        node = node.next;
+      } else break;
+    }
+    
+    if (node.value !== k && node.next === null) break;
+
+    if (prev) {
+      prev.next = node.next ? node.next : null;
+      if (prev.next) {
+        node = prev.next;
+      }
+    } else {
+      l = node.next;
+      node = l;
+    }
+    isNotDone = node.next ? true : false;
+  }
+  return l;
 }
 
 module.exports = {
